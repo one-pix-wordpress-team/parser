762d77751f49be3f3d1e2924e9a49e1488fe2345
Chloyka <chloyka@gmail.com>
2019-06-22 01:46:54 +0800

    Such commit
    Much perfect

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,55 +12,85 @@ jQuery(document).ready(function(){
 
   //add item action
 
-  jQuery('.items-row').on('click', '.test-connection', function(form){
+  jQuery('.items-row').on('submit', '.new-form', function(form){
+      form.preventDefault();
       var host = jQuery('.inp-host').val();
       var name = jQuery('.inp-username').val();
       var pass = jQuery('.inp-password').val();
       var path1 = jQuery('.inp-file-1').val();
       var path2 = jQuery('.inp-file-2').val();
       var path3 = jQuery('.inp-file-3').val();
+      var new_item = jQuery(this).parents('.new-parser-item');
       $.ajax({
           method: 'POST',
           url: '/handler.php',
           data: { action: 'addItem', host: host , username: name, password: pass, path1: path1, path2: path2, path3: path3}
       })
           .success(function(data) {
-              alert( "Data Saved: " + data );
-          });
-  jQuery('.items-row').append('<div class="parser-item w-100">' +
-      '<div class="row">' +
-      '<div class="col-lg-6 host-inner item-inner">'+host+'</div>' +
-      '<div class="col-lg-3 item-inner" style="color:#28a745;">' +
-      'Working' +
-      '</div>' +
-      '<div class="col-lg-3">' +
-      '<div class="remove-item btn btn-danger">-</div>' +
-      '</div>' +
-      '</div>' +
-      '</div>');
-      jQuery(this).parents('.new-parser-item').remove();
+              if(data == 'true') {
+                  alert("Adding a source was successful!");
+                  jQuery('.items-row').append('<div class="parser-item w-100">' +
+                      '<div class="row">' +
+                      '<div class="col-lg-6 host-inner item-inner">'+host+'</div>' +
+                      '<div class="col-lg-3 item-inner" style="color:#28a745;">' +
+                      'Working' +
+                      '</div>' +
+                      '<div class="col-lg-3">\n' +
+                      '                    <div class="item-info btn"><i class="fa fa-eye" aria-hidden="true"></i></div><div class="remove-item btn btn-danger">-</div>\n' +
+                      '                </div>\n' +
+                      '                <div style="display:none" class="popup-config"><div class="spoiler col-lg-12">\n' +
+                      '                        <div class="row">\n' +
+                      '                            <div class="close"><i class="fa fa-times" aria-hidden="true"></i></div>\n' +
+                      '                            <div class="popup-headers row">\n' +
+                      '                            <div class="col-lg-9 head-item">\n' +
+                      '                                File name\n' +
+                      '                            </div>\n' +
+                      '                            <div class="col-lg-3 head-item">\n' +
+                      '                                Status\n' +
+                      '                            </div>\n' +
+                      '                            </div>\n' +
+                      '                            <div class="row file-row">\n' +
+                      '                                <div class="col-lg-9 host-inner item-inner">'+path1+'</div>\n' +
+                      '                                <div class="col-lg-3 item-inner" style="color:#28a745;">\n' +
+                      '                                    Working\n' +
+                      '                                </div>\n' +
+                      '                            </div>\n' +
+                      '                            </div>\n' +
+                      '                        </div>\n' +
+                      '</div>\n' +
+                      '                    </div>');
+
+                  new_item.remove();
+              } else {
+                  alert("An error occurred! Please check your connection details!");
+              }
+
+          })
+
   });
 
 //delete item action
 
     jQuery('.items-row').on('click', '.remove-item', function(){
-        var host = jQuery(this).parents('.row').children('.host-inner').text();;
-        $.ajax({
+        var host = jQuery(this).parents('.row').children('.host-inner').text();
+        var here = jQuery(this).parents('.parser-item');
+
+            $.ajax({
             method: 'POST',
             url: '/handler.php',
             data: { action: 'deleteItem', host: host }
         })
             .success(function(data) {
-                alert('Success delete!');
-                jQuery(this).parents('.parser-item').remove();
+                if(data == 'true') {
+                    alert('The object was successfully deleted!');
+                    here.remove();
+                }
             });
-
-        jQuery(this).parents('.parser-item').remove();
     });
-jQuery('.item-info').on('click', function(){
+jQuery('.items-row').on('click', '.item-info', function(){
     jQuery(this).parents('.row').children('.popup-config').slideDown(200);
 })
-    jQuery('.close').on('click', function(){
+    jQuery('.items-row').on('click', '.close', function(){
         jQuery(this).parents('.popup-config').slideUp(200);
     })
 });
